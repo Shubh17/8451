c7d1f7b48ed906b88e2c765807cea6a7798e1fba
johnlk <jlkuhn@purdue.edu>
2017-08-27 13:17:29 -0400

    the product model has been build and now can be given products to buy

--- dataCleaning/recommendMe.py
+++ dataCleaning/recommendMe.py
@@ -2,4 +2,4 @@ import pickle
 
 loaded_model = pickle.load(open('productRec_model.sav', 'rb'))
 
-print loaded_model.predict([[1,2,3,4,5,6,7,8,9]])
+print loaded_model.predict([[27958,27958,27958,27958,27958,27958,27958,27958,27958]])
